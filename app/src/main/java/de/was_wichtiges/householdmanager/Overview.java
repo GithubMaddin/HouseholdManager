09ea592edb80b1aaa78c80f5a36bfad54dd0f667
jfmarten <jakob@brombeerweg.de>
2017-02-15 21:38:37 +0100

    Create simple overview layout with no functionality

--- app/src/main/java/de/was_wichtiges/householdmanager/Overview.java
+++ app/src/main/java/de/was_wichtiges/householdmanager/Overview.java
@@ -3,6 +3,9 @@ package de.was_wichtiges.householdmanager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * Overview Activity
+ */
 public class Overview extends AppCompatActivity {
 
     @Override
